d15f607ad97b9bde1223d5f4012fa5f383862232
NiltonRamosE <niltonencarnacion17@gmail.com>
2025-05-13 07:56:05 -0500

    navbar items  end and favicon resolve

--- src/partials/navbar.tsx
+++ src/partials/navbar.tsx
@@ -8,14 +8,14 @@ import {
 import { siteConfig } from "@/config/site";
 
 export const Navbar = () => {
-
   const handleScroll = (e: React.MouseEvent<HTMLAnchorElement>, id: string) => {
     e.preventDefault();
     const targetElement = document.getElementById(id);
 
     if (targetElement) {
       const navbarHeight = 80;
-      const elementPosition = targetElement.getBoundingClientRect().top + window.scrollY;
+      const elementPosition =
+        targetElement.getBoundingClientRect().top + window.scrollY;
       const offsetPosition = elementPosition - navbarHeight - 20;
 
       window.scrollTo({
@@ -27,11 +27,13 @@ export const Navbar = () => {
 
   return (
     <NavbarUI
-      className="fixed h-[70px] top-4 z-50 left-1/2 transform -translate-x-1/2 shadow-md rounded-full max-w-sm sm:max-w-lg md:max-w-2xl lg:max-w-3xl border-2 border-nilton-neon bg-white/30 backdrop-blur-md" 
+      className="fixed h-[70px] top-4 z-50 left-1/2 transform -translate-x-1/2 shadow-md 
+             rounded-full max-w-sm sm:max-w-lg md:max-w-2xl lg:max-w-3xl 
+             border-2 border-nilton-neon bg-white/30 backdrop-blur-md"
     >
-      <NavbarContent>
+      <NavbarContent className="flex-1">
         <NavbarBrand>
-          <a 
+          <a
             className="font-bold text-inherit flex items-center gap-2"
             color="foreground"
             href="#hero"
@@ -46,15 +48,17 @@ export const Navbar = () => {
         </NavbarBrand>
       </NavbarContent>
 
-      <NavbarContent className="" justify="end">
+      <NavbarContent justify="end">
         {siteConfig.navItems.map((item) => (
           <NavbarItem key={item.id}>
-            <a 
+            <a
               href={item.href}
               onClick={(e) => handleScroll(e, item.href.replace("#", ""))}
-              className="sm:mr-4 lg:mr-10 font-bold text-sm md:text-lg transition-colors hover:text-nilton-green"
+              className="sm:mr-4 lg:mr-10 font-bold text-sm md:text-lg transition-colors hover:text-black/80"
               color="foreground"
-            >{item.label}</a>
+            >
+              {item.label}
+            </a>
           </NavbarItem>
         ))}
       </NavbarContent>
